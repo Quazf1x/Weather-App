743bb75d990c12457967a79f5fdead72faf326a6
Quazf1x <103037316+Quazf1x@users.noreply.github.com>
2023-06-07 14:29:14 +0600

    Ver 0.0.7

---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+const button = document.querySelector('#find-city-button');
+const input = document.querySelector('#find-city-input');
+
+button.addEventListener('click', () => {
+  input.focus();
+});
